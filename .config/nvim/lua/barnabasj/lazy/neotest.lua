539bebf13de862ea73a03dc28e66c5fbb9e5c2f2
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2024-07-04 09:28:01 +0200

    update neovim plugins

--- .config/nvim/lua/barnabasj/lazy/neotest.lua
+++ .config/nvim/lua/barnabasj/lazy/neotest.lua
@@ -2,6 +2,7 @@ return {
     {
         "nvim-neotest/neotest",
         dependencies = {
+            "nvim-neotest/nvim-nio",
             "nvim-lua/plenary.nvim",
             "antoinemadec/FixCursorHold.nvim",
             "nvim-treesitter/nvim-treesitter",
@@ -31,4 +32,3 @@ return {
         end,
     },
 }
-
