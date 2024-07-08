6522a3f077cda4af053a8e6203e459aa533e4737
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2024-07-08 11:14:01 +0200

    add resume to telescope bindings

--- private_dot_config/nvim/lua/barnabasj/lazy/telescope.lua
+++ private_dot_config/nvim/lua/barnabasj/lazy/telescope.lua
@@ -1,40 +1,41 @@
 -- requires ripgrep and fzf to be installed
 return {
-    "nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+	tag = "0.1.5",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 
-    config = function()
-        require("telescope").setup({})
+	config = function()
+		require("telescope").setup({})
 
-        local builtin = require("telescope.builtin")
+		local builtin = require("telescope.builtin")
 
-        local wk = require("which-key")
+		local wk = require("which-key")
 
-        wk.register({
-            t = {
-                name = "Telescope",
-                f = { builtin.find_files, "file" },
-                g = { builtin.git_files, "git file" },
-                w = { builtin.grep_string, "word" },
-                s = {
-                    function()
-                        builtin.grep_string({ search = vim.fn.input("Grep for > ") })
-                    end,
-                    "search",
-                },
-                l = { builtin.live_grep, "word live" },
-                t = {
-                    names = "Tags",
-                    h = { builtin.help_tags, "help" },
-                },
-            },
-        }, { prefix = "<leader>" })
+		wk.register({
+			t = {
+				name = "Telescope",
+				f = { builtin.find_files, "file" },
+				g = { builtin.git_files, "git file" },
+				w = { builtin.grep_string, "word" },
+				s = {
+					function()
+						builtin.grep_string({ search = vim.fn.input("Grep for > ") })
+					end,
+					"search",
+				},
+				l = { builtin.live_grep, "word live" },
+				t = {
+					names = "Tags",
+					h = { builtin.help_tags, "help" },
+				},
+				r = { builtin.resume, "resume" },
+			},
+		}, { prefix = "<leader>" })
 
-        vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-    end,
+		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+	end,
 }
