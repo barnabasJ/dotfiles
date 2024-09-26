94980e1a10e32485773c472460ce30b53c36a7e5
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2024-09-26 19:33:53 +0200

    more nvim config changes

--- private_dot_config/nvim/lua/barnabasj/lazy/colors.lua
+++ private_dot_config/nvim/lua/barnabasj/lazy/colors.lua
@@ -1,25 +1,33 @@
 return {
-    {
-        -- https://github.com/folke/tokyonight.nvim
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-            styles = {
-                -- Style to be applied to different syntax groups
-                -- Value is any valid attr-list value for `:help nvim_set_hl`
-                comments = { italic = false },
-                keywords = { italic = false },
-                -- Background styles. Can be "dark", "transparent" or "normal"
-                sidebars = "dark", -- style for sidebars, see below
-                floats = "dark", -- style for floating windows
-            },
-        },
-        init = function()
-            vim.cmd.colorscheme("tokyonight-night")
-        end,
-    },
+	-- {
+	-- 	-- https://github.com/folke/tokyonight.nvim
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {
+	-- 		-- your configuration comes here
+	-- 		-- or leave it empty to use the default settings
+	-- 		terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+	-- 		styles = {
+	-- 			-- Style to be applied to different syntax groups
+	-- 			-- Value is any valid attr-list value for `:help nvim_set_hl`
+	-- 			comments = { italic = false },
+	-- 			keywords = { italic = false },
+	-- 			-- Background styles. Can be "dark", "transparent" or "normal"
+	-- 			sidebars = "dark", -- style for sidebars, see below
+	-- 			floats = "dark", -- style for floating windows
+	-- 		},
+	-- 	},
+	-- 	init = function()
+	-- 		vim.cmd.colorscheme("tokyonight-night")
+	-- 	end,
+	-- },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		init = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
 }
--- private_dot_config/nvim/lua/barnabasj/remap.lua
+++ private_dot_config/nvim/lua/barnabasj/remap.lua
@@ -1,21 +1,21 @@
 local wk = require("which-key")
 -- config
 wk.add({
-    {
-        "<leader><leader>",
-        function()
-            vim.cmd("so")
-        end,
-        desc = "Reload config",
-    },
+	{
+		"<leader>rc",
+		function()
+			vim.cmd("source $MYVIMRC")
+		end,
+		desc = "[R]eload [c]onfig",
+	},
 })
 -- netrw
 wk.add({
-    { "<leader>f",  group = "File" },
-    { "<leader>fe", vim.cmd.Ex,    desc = "Open explorer" },
+	{ "<leader>f", group = "File" },
+	{ "<leader>fe", vim.cmd.Ex, desc = "Open explorer" },
 })
 
 -- convenience
 wk.add({
-    { "<ESC>", "<cmd>nohlsearch<CR>", desc = "clear hl search" },
+	{ "<ESC>", "<cmd>nohlsearch<CR>", desc = "clear hl search" },
 })
