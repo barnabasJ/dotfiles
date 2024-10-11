e8f8ecc9cefa0d439eb5f47901c13de3a8900d41
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2024-10-11 12:48:47 +0200

    add aliases for tabs

--- private_dot_config/nvim/lua/barnabasj/remap.lua
+++ private_dot_config/nvim/lua/barnabasj/remap.lua
@@ -15,6 +15,16 @@ wk.add({
 	{ "<leader>fe", vim.cmd.Ex, desc = "Open explorer" },
 })
 
+-- tabs
+wk.add({
+	{ "<leader>tn", vim.cmd.tabnew, desc = "New tab" },
+	{ "<leader>tc", vim.cmd.tabclose, desc = "Close tab" },
+	{ "<leader>to", vim.cmd.tabonly, desc = "Close other tabs" },
+	{ "<leader>tl", vim.cmd.tabnext, desc = "Next tab" },
+	{ "<leader>th", vim.cmd.tabprevious, desc = "Previous tab" },
+	{ "<leader>tm", vim.cmd.tabmove, desc = "Move tab" },
+})
+
 -- convenience
 wk.add({
 	{ "<ESC>", "<cmd>nohlsearch<CR>", desc = "clear hl search" },
