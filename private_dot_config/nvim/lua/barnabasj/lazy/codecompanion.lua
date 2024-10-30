f3697cb04ca28d000a0b9c0a99df832f52073842
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2024-10-30 19:37:22 +0100

    add codecompanion

---
+++ private_dot_config/nvim/lua/barnabasj/lazy/codecompanion.lua
@@ -0,0 +1,30 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+		{ "nvim-lua/plenary.nvim" },
+		{ "hrsh7th/nvim-cmp" },
+		{ "stevearc/dressing.nvim", opts = {} },
+		{ "nvim-telescope/telescope.nvim" },
+	},
+	opts = {
+		--Refer to: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
+		display = {
+			diff = {
+				provider = "mini_diff",
+			},
+		},
+		strategies = {
+			--NOTE: Change the adapter as required
+			chat = {
+				adapter = "ollama",
+			},
+			inline = {
+				adapter = "copilot",
+			},
+		},
+		opts = {
+			log_level = "DEBUG",
+		},
+	},
+}
