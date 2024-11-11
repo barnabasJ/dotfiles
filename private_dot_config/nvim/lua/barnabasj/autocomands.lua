75ac95807074fa84265032e6c70edb6647d8b30a
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2024-11-11 23:14:46 +0100

    set filetype on mjml files to html

--- private_dot_config/nvim/lua/barnabasj/autocomands.lua
+++ private_dot_config/nvim/lua/barnabasj/autocomands.lua
@@ -3,22 +3,29 @@
 
 -- open all folds when opening a file
 vim.api.nvim_create_autocmd("BufWinEnter", {
-    pattern = "*",
-    callback = function()
-        vim.cmd("silent! :%foldopen!")
-    end,
+	pattern = "*",
+	callback = function()
+		vim.cmd("silent! :%foldopen!")
+	end,
 })
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = "IncSearch",
-            timeout = 80,
-        })
-    end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 80,
+		})
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "*.mjml",
+	callback = function()
+		vim.bo.filetype = "html"
+	end,
 })
