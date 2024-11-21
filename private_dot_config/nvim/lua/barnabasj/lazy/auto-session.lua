4daa0a84673a778331342c3ea5b35debc6b66773
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2024-11-21 13:57:44 +0100

    add keys for autosession

--- private_dot_config/nvim/lua/barnabasj/lazy/auto-session.lua
+++ private_dot_config/nvim/lua/barnabasj/lazy/auto-session.lua
@@ -1,7 +1,12 @@
 return {
 	"rmagatti/auto-session",
 	lazy = false,
-
+	keys = {
+		-- Will use Telescope if installed or a vim.ui.select picker otherwise
+		{ "<leader>wr", "<cmd>SessionSearch<CR>", desc = "Session search" },
+		{ "<leader>ws", "<cmd>SessionSave<CR>", desc = "Save session" },
+		{ "<leader>wa", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
+	},
 	---enables autocomplete for opts
 	---@module "auto-session"
 	---@type AutoSession.Config
