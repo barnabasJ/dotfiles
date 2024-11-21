a1369f95af061b0d751166f78dc6381970ce9cc9
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2024-11-22 00:11:19 +0100

    add a keybind to focus undo tree tab

--- private_dot_config/nvim/lua/barnabasj/lazy/undotree.lua
+++ private_dot_config/nvim/lua/barnabasj/lazy/undotree.lua
@@ -3,5 +3,6 @@ return {
 
 	config = function()
 		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle undotree" })
+		vim.keymap.set("n", "<leader>uf", vim.cmd.UndotreeFocus, { desc = "Focus undotree" })
 	end,
 }
