f7dda95902445e4ef2d6f5b5456d909310fbb377
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-03-13 03:03:32 +0100

    do not load better white space on dashboard

--- private_dot_config/nvim/lua/barnabasj/lazy/better-white-space.lua
+++ private_dot_config/nvim/lua/barnabasj/lazy/better-white-space.lua
@@ -2,4 +2,14 @@ return {
 	-- https://github.com/ntpeters/vim-better-whitespace
 	-- Highlights trailing whitespace
 	"ntpeters/vim-better-whitespace",
+	event = "BufReadPre",
+	init = function()
+		-- Disable for dashboard
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "dashboard",
+			callback = function()
+				vim.b.better_whitespace_enabled = 0
+			end,
+		})
+	end,
 }
