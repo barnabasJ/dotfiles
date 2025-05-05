11f9b04edf3e1863cc75d3a33d98edfd35817f59
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-05-05 21:15:22 +0200

    add lazydev for better lua lsp support in nvim configs

--- private_dot_config/nvim/lua/barnabasj/lazy/lazydev.lua
+++ private_dot_config/nvim/lua/barnabasj/lazy/lazydev.lua
@@ -1,25 +1,14 @@
+---@type LazySpec
 return {
-	{
-		"folke/lazydev.nvim",
-		ft = "lua", -- only load on lua files
-		opts = {
-			library = {
-				-- See the configuration section for more details
-				-- Load luvit types when the `vim.uv` word is found
-				{ path = "luvit-meta/library", words = { "vim%.uv" } },
-			},
+	"folke/lazydev.nvim",
+	ft = "lua", -- only load on lua files
+	opts = {
+		library = {
+			"lazy.nvim",
+			"snacks.nvim",
+			-- See the configuration section for more details
+			-- Load luvit types when the `vim.uv` word is found
+			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 		},
 	},
-	{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-	-- TODO: check if this actually does anything
-	-- { -- optional completion source for require statements and module annotations
-	-- 	"hrsh7th/nvim-cmp",
-	-- 	opts = function(_, opts)
-	-- 		opts.sources = opts.sources or {}
-	-- 		table.insert(opts.sources, {
-	-- 			name = "lazydev",
-	-- 			group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-	-- 		})
-	-- 	end,
-	-- },
 }
