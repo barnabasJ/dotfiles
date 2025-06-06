b4b424d264c8b63e67360de72d84546e20e6df9c
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2025-06-07 00:00:04 +0200

    Fix typescript-tools to use blink.cmp capabilities
    Replace cmp_nvim_lsp with blink.cmp.get_lsp_capabilities() in typescript-tools configuration

--- private_dot_config/nvim/lua/barnabasj/lazy/typescript-tools.lua
+++ private_dot_config/nvim/lua/barnabasj/lazy/typescript-tools.lua
@@ -1,9 +1,8 @@
-local cmp_lsp = require("cmp_nvim_lsp")
 local capabilities = vim.tbl_deep_extend(
 	"force",
 	{},
 	vim.lsp.protocol.make_client_capabilities(),
-	cmp_lsp.default_capabilities(),
+	require("blink.cmp").get_lsp_capabilities(),
 	{
 		documentFormattingProvider = false,
 		documentRangeFormattingProvider = false,
