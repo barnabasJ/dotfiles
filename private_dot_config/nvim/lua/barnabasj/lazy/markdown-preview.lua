f6db9d51d254db41b4609be2fbeb4c47ab106f68
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2026-08-29 17:26:01 +0200

    fix fresh Neovim plugin restoration

--- private_dot_config/nvim/lua/barnabasj/lazy/markdown-preview.lua
+++ private_dot_config/nvim/lua/barnabasj/lazy/markdown-preview.lua
@@ -2,7 +2,16 @@ return {
 	"iamcco/markdown-preview.nvim",
 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 	ft = { "markdown" },
-	build = function()
-		vim.fn["mkdp#util#install"]()
+	build = function(plugin)
+		-- lazy.nvim runs build callbacks before a freshly cloned plugin is on
+		-- runtimepath. Add only this directory while invoking markdown-preview's
+		-- documented installer, then restore runtimepath even if it fails.
+		-- https://github.com/iamcco/markdown-preview.nvim#install-with-lazynvim
+		vim.opt.runtimepath:append(plugin.dir)
+		local ok, err = pcall(vim.fn["mkdp#util#install"], true)
+		vim.opt.runtimepath:remove(plugin.dir)
+		if not ok then
+			error(err)
+		end
 	end,
 }
